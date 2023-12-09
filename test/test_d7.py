682931cda9e92ac1c744ca7f08b6961218a945c1
pavzari <92022046+pavzari@users.noreply.github.com>
2023-12-09 16:05:19 +0000

    ...

--- test/test_d7.py
+++
@@ -1,7 +0,0 @@
-from d7 import total_winnings
-
-input = [line.strip() for line in open("input/d7_sample", "r").readlines()]
-
-
-def test_d7_a():
-    assert total_winnings(input) == 6440
--- test/test_d9.py
+++
@@ -1,7 +0,0 @@
-from d9 import sum_oasis_values
-
-input = [line.strip() for line in open("input/d9_sample", "r").readlines()]
-
-
-def test_d9_a():
-    assert sum_oasis_values(input) == 114
